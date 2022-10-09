c0dd555c83c0882b3e1f62609a0e29e528643315
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2022-10-08 20:53:36 -0700

    Add initial options and input reading

--- Gaussian.java
+++ Gaussian.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 class Gaussian
 {
     public static void main(String[] args)
     {
+        System.out.println("\nGAUSSIAN ELIMINATION WITH SCALED PARTIAL PIVOTING");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number of equations: ");
+        int equations = scan.nextInt();
+        System.out.println("\nWould you like to:\n1. Enter the coefficients from the command line\n2. Read a file");
+        int option = scan.nextInt();
 
     }
 }
